9897eddb3fd6cd7ec59a51eaabbcd16b6f155153
Vicky Kumar <imvickykumar999@gmail.com>
2024-08-03 09:52:37 +0530

    Update settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -106,11 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_TZ = True
+
+USE_I18N = True
 
 
 # Static files (CSS, JavaScript, Images)
